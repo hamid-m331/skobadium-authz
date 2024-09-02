71d830e54fce89b6b4991b72262b7dc57bc014b9
Hamid Madahian <hamid.m.331@gmail.com>
2024-09-02 18:49:45 +0330

    adding flask sqlalchemy package

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 from authz.config import Config
 
+db = SQLAlchemy()
 api = Api()
 
 from authz import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
     api.init_app(app)
     return app
--- authz/config/config.py
+++ authz/config/config.py
@@ -1,6 +1,14 @@
 from os import environ
 
 class Config:
+    ###  GLOBAL CONFIGs  ###
+
     ENV = environ.get("SKOB_AUTHZ_ENV", "production")
     DEBUG = int(environ.get("SKOB_AUTHZ_DEBUG", "0"))
     TESTING = int(environ.get("SKOB_AUTHZ_TESTING", "0"))
+
+    ###  DATABASE CONFIG  ###
+
+    SQLALCHEMY_DATABASE_URI = environ.get("SKOB_AUTHZ_DATABASE_URI", None)
+    SQLALCHEMY_TRACK_MODIFICATIONS = TESTING
+
