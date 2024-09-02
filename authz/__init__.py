e7456f9eb18a8765b9beb5a052a2344f44db01d3
Hamid Madahian <hamid.m.331@gmail.com>
2024-09-02 10:00:46 +0330

    adding restfull package

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_restful import Api
 from authz.config import Config
 
+api = Api()
+
+from authz import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    api.init_app(app)
     return app
